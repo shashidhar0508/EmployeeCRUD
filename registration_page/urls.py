3b08fe29fd65d637ea760e7f63575412186d68f8
shashidhar0508 <shashidhar0508@gmail.com>
2020-11-02 20:59:31 +0530

    committed with changes in update employee

--- registration_page/urls.py
+++ registration_page/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.showallemployees,name="showallemployees"),
-    path('Insert', views.Insertemp,name="Insertemp")
+    path('Insert', views.Insertemp,name="Insertemp"),
+    path('delete/<str:id>', views.delete,name="delete"),
+    path('edit/<str:id>', views.edit,name="edit"),
+    path('edit/update/<str:id>', views.update,name="update"),
 ]
--- registration_page/views.py
+++ registration_page/views.py
@@ -7,8 +7,8 @@ from django.contrib import messages
 def showallemployees(request):
     allemployees=EmpModel.objects.all()
     print("allempdata : ",allemployees)
-    print(type(allemployees))
-    print(len(allemployees))
+    print("type of allemployees : ",type(allemployees))
+    print("length of allemployees : ",len(allemployees))
     return render(request,"index.html",{"allempdata":allemployees})
 
 def Insertemp(request):
@@ -16,6 +16,7 @@ def Insertemp(request):
         if request.POST.get('eid') and request.POST.get('ename') and request.POST.get('email') and request.POST.get('econtact'):
             saveemp=EmpModel()
             saveemp.eid=request.POST.get('eid')
+            print("eid from Insertemp : ",request.POST.get('eid'))
             saveemp.ename=request.POST.get('ename')
             saveemp.email=request.POST.get('email')
             saveemp.econtact=request.POST.get('econtact')
@@ -26,3 +27,30 @@ def Insertemp(request):
             return render(request, "Insert.html")
     else:
             return render(request, "Insert.html")
+
+def edit(request,id):
+    employee=EmpModel.objects.get(eid=id)
+    print("employee : ",employee)
+    return render(request,"edit.html",{"employee":employee})
+
+def update(request,id):
+    employee = EmpModel.objects.get(eid=id)
+    print("employee : ", employee)
+    if request.POST.get('eid') and request.POST.get('ename') and request.POST.get('email') and request.POST.get(
+            'econtact'):
+        saveemp = EmpModel()
+        saveemp.eid = request.POST.get('eid')
+        print("eid from Insertemp : ", request.POST.get('eid'))
+        saveemp.ename = request.POST.get('ename')
+        saveemp.email = request.POST.get('email')
+        saveemp.econtact = request.POST.get('econtact')
+        saveemp.save()
+        return redirect('/')
+    else:
+        return render(request, "edit.html")
+
+def delete(request,id):
+    print("employee id : ",id)
+    employee=EmpModel.objects.get(eid=id)
+    employee.delete()
+    return redirect('/')
